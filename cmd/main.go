e91bc378d1b84f60f9bc30f466c3c4eb0616bed0
in-jun <injuninjune@gmail.com>
2024-09-17 01:34:38 +0900

    chore: rewrite codebase from scratch

--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/injunweb/backend-server/internal/server"
-
-func main() {
-	server.Run()
-}
--- internal/domain/admin/entities/admin.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-import "gorm.io/gorm"
-
-type Admin struct {
-	gorm.Model
-	AdminName string `gorm:"not null;unique"`
-	Password  string `gorm:"not null"`
-}
