e77eaa2b6415a0e918220efdb6b5073c6fdffca7
in-jun <injuninjune@gmail.com>
2024-09-25 22:24:10 +0900

    fix: Local 수정

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ type ErrorResponseDTO struct {
 func init() {
 	// Connect to database
 	var err error
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FSeoul",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
