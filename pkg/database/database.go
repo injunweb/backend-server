6f87c069b1807cf3765e90ff3c2b55cd5c3f86e5
in-jun <injuninjune@gmail.com>
2024-10-15 20:04:00 +0900

    feat: Notification테이블 AutoMigrate

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -29,7 +29,7 @@ func Init() error {
 		return fmt.Errorf("failed to connect to database: %v", err)
 	}
 
-	err = DB.AutoMigrate(&models.User{}, &models.Application{}, &models.ExtraHostnames{})
+	err = DB.AutoMigrate(&models.User{}, &models.Application{}, &models.ExtraHostnames{}, &models.Notification{})
 	if err != nil {
 		return fmt.Errorf("failed to migrate database: %v", err)
 	}
