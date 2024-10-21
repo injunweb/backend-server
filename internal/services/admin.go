a0f204cbf7fef04be43e07b4fea610367cdaa956
in-jun <injuninjune@gmail.com>
2024-10-21 13:39:26 +0900

    feat: GetAllApplicationsByAdmin에 Preload추가

--- internal/services/admin.go
+++ internal/services/admin.go
@@ -117,7 +117,7 @@ type GetAllApplicationsByAdminResponse struct {
 
 func (s *AdminService) GetAllApplicationsByAdmin() (GetAllApplicationsByAdminResponse, error) {
 	var applications []models.Application
-	if err := s.db.Order("created_at DESC").Find(&applications).Error; err != nil {
+	if err := s.db.Preload("Owner").Order("created_at DESC").Find(&applications).Error; err != nil {
 		return GetAllApplicationsByAdminResponse{}, errors.New("failed to retrieve applications")
 	}
 
